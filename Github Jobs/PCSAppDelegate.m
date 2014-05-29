fa93cdcf0848a7c2d18bb64215c8304ecf5a6eb4
Romain Pouclet <palleas@gmail.com>
2014-05-28 23:40:03 -0400

    LOL hi

--- Github Jobs/PCSAppDelegate.m
+++ Github Jobs/PCSAppDelegate.m
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    NSLog(@"Hi, I’m a book created for a sample application");
+    NSLog(@"LOL-Hi, I’m a book created for a sample application");
 
     return YES;
 }
