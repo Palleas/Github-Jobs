51cb6c5214f1aeca3059e2f6615150800a145457
Romain Pouclet <palleas@gmail.com>
2014-05-28 23:19:46 -0400

    Saying hi to the developer

--- Github Jobs/PCSAppDelegate.m
+++ Github Jobs/PCSAppDelegate.m
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-
+    NSLog(@"Hi, I'm a sample application for a book.");
 
     return YES;
 }
